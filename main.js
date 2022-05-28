3d66275a38fa11bab5b05e85c0b9bee260f783cc
QiuFilms <qiufilms@gmail.com>
2022-05-28 13:47:18 +0200

    update

--- main.js
+++ main.js
@@ -22,19 +22,19 @@ fs.readFile(__filename, 'utf8' , (err, data) => {
     for(i in array.body){
         if(array.body[i].type=="VariableDeclaration"){
             if((array.body[i].declarations[0].id.name).includes("__STRING__")){
-                console.log(array.body[i].declarations[0].id.name)
+                //console.log(array.body[i].declarations[0].id.name)
                 if(typeof array.body[i].declarations[0].init.value != "string"){
                     throw new Error(`Assignment of non string value to string type ${array.body[i].declarations[0].id.name}`)
                 }
             }else if((array.body[i].declarations[0].id.name).includes("__INT__")){
-                console.log(array.body[i].declarations[0].id.name)
+                //console.log(array.body[i].declarations[0].id.name)
                 if(typeof array.body[i].declarations[0].init.value != "number"){
                     throw new Error(`Assignment of non integer value to integer type ${array.body[i].declarations[0].id.name}`)
                 }else if(!isInteger(array.body[i].declarations[0].init.value)){
                     throw new Error(`Assignment of non integer value to integer type ${array.body[i].declarations[0].id.name}`)
                 }
             }else if((array.body[i].declarations[0].id.name).includes("__FLOAT__")){
-                console.log(array.body[i].declarations[0].id.name)
+                //console.log(array.body[i].declarations[0].id.name)
                 if(typeof array.body[i].declarations[0].init.value != "number"){
                     throw new Error(`Assignment of non float value to float type: ${array.body[i].declarations[0].id.name}`)
                 }else if(!isFloat(array.body[i].declarations[0].init.value)){
@@ -43,15 +43,41 @@ fs.readFile(__filename, 'utf8' , (err, data) => {
             }
 
         }
+
+        if(array.body[i].type=="ExpressionStatement"){
+            if(array.body[i].expression.type == "AssignmentExpression"){
+                if((array.body[i].expression.left.name).includes("__STRING__")){
+                    console.log(array.body[i].expression.left.name)
+                    if(typeof array.body[i].expression.right.value != "string"){
+                        throw new Error(`Assignment of non string value to string type ${array.body[i].expression.left.name}`)
+                    }
+                }else if((array.body[i].expression.left.name).includes("__INT__")){
+                    if(typeof array.body[i].expression.right.value != "number"){
+                        throw new Error(`Assignment of non integer value to integer type ${array.body[i].expression.left.name}`)
+                    }else if(!isInteger(array.body[i].expression.right.value)){
+                        throw new Error(`Assignment of non integer value to integer type ${array.body[i].expression.left.name}`)
+                    }
+                }else if((array.body[i].expression.left.name).includes("__FLOAT__")){
+                    if(typeof array.body[i].expression.right.value != "number"){
+                        throw new Error(`Assignment of non float value to float type: ${array.body[i].expression.left.name}`)
+                    }else if(!isFloat(array.body[i].expression.right.value)){
+                        throw new Error(`Assignment of non float value to float type: ${array.body[i].expression.left.name}`)
+                    }
+                }
+            }
+        }
     }
 })
 
 var variables = ""
 var name__STRING__ = "12"
 var name__INT__ = 123
-var name__FLOAT__ = 10
+var name__FLOAT__ = 10.1
 var abb = 10.1
 
+name__STRING__ = "asd"
+name__INT__ = 123
+name__FLOAT__ = 10
 
 
 function asd(){
